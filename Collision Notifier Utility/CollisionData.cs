852066df32903a6bb84895b8ea895599eaabc0a1
imasads <38746430+imasads@users.noreply.github.com>
2018-09-18 16:31:47 +0500

    Add files via upload

---
+++ Collision Notifier Utility/CollisionData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionData : MonoBehaviour
+{
+    private object collisionInfo;
+
+    public object CollisionInfo
+    {
+        get
+        {
+            return collisionInfo;
+        }
+
+        set
+        {
+            collisionInfo = value;
+        }
+    }
+}
---
+++ Collision Notifier Utility/CollisionNotifier.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(CollisionData))]
+public class CollisionNotifier : MonoBehaviour
+{
+    private CollisionData collisionData;
+
+    [Header("OnTrigger Events")]
+    public UnityEvent OnTriggerEnterEvent;
+    public UnityEvent OnTriggerExitEvent;
+
+    [Header("OnCollision Events")]
+    public UnityEvent OnCollisionEnterEvent;
+    public UnityEvent OnCollisionExitEvent;
+
+    void Awake()
+    {
+        collisionData = GetComponent<CollisionData>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        collisionData.CollisionInfo = other;
+
+        if (OnTriggerEnterEvent != null)
+        {
+            OnTriggerEnterEvent.Invoke();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        collisionData.CollisionInfo = other;
+
+        if (OnTriggerExitEvent != null)
+        {
+            OnTriggerExitEvent.Invoke();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collisionData.CollisionInfo = collision;
+
+        if (OnCollisionEnterEvent != null)
+        {
+            OnCollisionEnterEvent.Invoke();
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        collisionData.CollisionInfo = collision;
+
+        if (OnCollisionExitEvent != null)
+        {
+            OnCollisionExitEvent.Invoke();
+        }
+    }
+}
